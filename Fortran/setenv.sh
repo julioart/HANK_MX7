db9a00bd0b60e489612113843fb7ace18a06421b
Julio <jcarrillo@banxico.org.mx>
2019-10-11 10:25:40 -0500

    Update setenv.sh

--- Fortran/setenv.sh
+++ Fortran/setenv.sh
@@ -5,7 +5,7 @@ source /opt/intel/bin/compilervars.sh intel64
 source /opt/intel/compilers_and_libraries_2019.3.199/linux/bin/compilervars.sh intel64
 
 echo 'ulimit -s unlimited'
-export OMP_NUM_THREADS = 30
+#export OMP_NUM_THREADS = 30
 echo 'NUM THREADS  ' $OMP_NUM_THREADS
 ulimit -s unlimited
 make clean
